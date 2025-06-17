0ca85dc0d941c4f11a59ac4208813d9f998e6bde
Ronald de Groot <ronald.de.groot@hva.nl>
2025-06-17 16:58:45 +0200

    Hotfix to storeOne reference

--- src/main/java/launcher/LauncherVanRonald.java
+++ src/main/java/launcher/LauncherVanRonald.java
@@ -9,8 +9,6 @@ import view.Main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class LauncherVanRonald {
