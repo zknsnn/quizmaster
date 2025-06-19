681f9e07a3319849e7f20607b2f3c8a78df55865
Dianne <dianne.van.der.craats@hva.nl>
2025-06-19 08:57:34 +0200

    Begin aan CreateUpdateQuiz.fxml + controller.

--- src/main/java/controller/CreateUpdateQuizController.java
+++ src/main/java/controller/CreateUpdateQuizController.java
@@ -4,6 +4,12 @@ import model.Quiz;
 
 public class CreateUpdateQuizController {
 
+    //    Quizzen CRUD Als coördinator wil ik de volledige CRUD-functionaliteit voor het beheer van
+//    quizzen hebben behorende bij cursussen waarvoor ik de rol coördinator heb (schermen
+//    manageQuizzes.fxml, createUpdateQuiz.fxml). Als ik een quiz selecteer (scherm
+//    manageQuizzes.fxml) wil ik meteen kunnen zien hoeveel vragen er in de quiz zitten.
+
+
     public void setup(Quiz quiz) {}
 
     public void doMenu() {}
