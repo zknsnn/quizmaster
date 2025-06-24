4f8f8a962796de41fb2f3c92c8681b0af95905ef
Ronald de Groot <ronald.de.groot@hva.nl>
2025-06-24 17:16:17 +0200

    CRUD Stuff werkend gekregen, with a little help...

--- src/main/java/controller/CreateUpdateQuestionController.java
+++ src/main/java/controller/CreateUpdateQuestionController.java
@@ -5,29 +5,27 @@ import database.mysql.QuizDAO;
 import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.stage.Popup;
-import javafx.stage.Stage;
 import javafx.util.Duration;
+import javafx.util.StringConverter;
 import model.Question;
 import model.Quiz;
-import model.User;
 import view.Main;
+
+import java.util.List;
 
 
 public class CreateUpdateQuestionController {
     private final QuestionDAO questionDAO = new QuestionDAO(Main.getDBAccess());
-    private User loggedInUser;
-    private QuizDAO quizDAO;
-    private Question question;
-    private Question selectedQuestion;
-    private Question questionId;
+    private QuizDAO quizDAO = new QuizDAO(Main.getDBAccess());
+    private Question currentQuestion;
 
     @FXML
     private Label titelLabel;
     @FXML
-    private TextField quizNaamTextField;
+    private ComboBox<Quiz> quizComboBox;
     @FXML
     private TextField questionNaamTextfield;
     @FXML
@@ -38,130 +36,124 @@ public class CreateUpdateQuestionController {
     private TextField wrongAnswer2Textfield;
     @FXML
     private TextField wrongAnswer3Textfield;
+    @FXML
+    private Label warningField;
 
     public void setup(Question question) {
-        this.loggedInUser = loggedInUser;
-        this.quizDAO = new QuizDAO(Main.getDBAccess());
-
+        currentQuestion = question;
+        fillComboBox();
+        convertQuizToString();
         if (question != null) {
-            this.selectedQuestion = question;
             haalInformatieQuestionOp(question);
-        } else {
-            this.selectedQuestion = null;
         }
     } // end setup
 
     public void doMenu() {
-        Main.getSceneManager().showWelcomeScene(loggedInUser);
+        Main.getSceneManager().showWelcomeScene(Main.currentUser());
     }
 
     public void doCreateUpdateQuestion() {
-        if (selectedQuestion == null) {
+        if (checkForEmptyFields()){
+            return;
+        }
+        if (currentQuestion == null) {
             createQuestion();
         } else {
             updateQuestion();
         }
-//
-//        if(currentquestion != null){
-//            vulalleveldenmetdeinhoud
-//        }
-
     } // end doCreateUpdateQuestion
 
     private Question updateQuestion() {
-        haalInformatieQuestionOp(selectedQuestion);
-        // Haal questionId op
-        Stage primaryStage = null;
-        questionId = questionDAO.getOneById(questionId.getQuestionId());
-        String quizNaam = quizNaamTextField.getText();
+        haalInformatieQuestionOp(currentQuestion);
+        String quizNaam = quizComboBox.getSelectionModel().getSelectedItem().getQuizName();
         Quiz quiz = quizDAO.getQuizPerID(quizNaam);
         // Gegevens bijwerken
-        question.setQuestionText(selectedQuestion.getQuestionText());
-        question.setCorrectAnswer(selectedQuestion.getCorrectAnswer());
-        question.setWrongAnswer1(selectedQuestion.getWrongAnswer1());
-        question.setWrongAnswer2(selectedQuestion.getWrongAnswer2());
-        question.setWrongAnswer3(selectedQuestion.getWrongAnswer3());
-        questionDAO.updateQuestion(question);
-        showAutoClosingWarning(primaryStage, "Vraag bijgewerkt.");
-        return question;
+        currentQuestion.setQuestionText(currentQuestion.getQuestionText());
+        currentQuestion.setCorrectAnswer(currentQuestion.getCorrectAnswer());
+        currentQuestion.setWrongAnswer1(currentQuestion.getWrongAnswer1());
+        currentQuestion.setWrongAnswer2(currentQuestion.getWrongAnswer2());
+        currentQuestion.setWrongAnswer3(currentQuestion.getWrongAnswer3());
+        questionDAO.updateQuestion(currentQuestion);
+        showWarning("Vraag bijgewerkt.");
+        return currentQuestion;
     } // updateQuestion
 
     private Question createQuestion() {
+        titelLabel.setText("Nieuwe vraag");
         String questionText = questionNaamTextfield.getText();
         String correctAnswer = correctAnswerTextfield.getText();
         String wrongAnswer1 = wrongAnswer1Textfield.getText();
         String wrongAnswer2 = wrongAnswer2Textfield.getText();
         String wrongAnswer3 = wrongAnswer3Textfield.getText();
-        String quizNaam = quizNaamTextField.getText();
+        String quizNaam = quizComboBox.getSelectionModel().getSelectedItem().getQuizName();
         Quiz quiz = quizDAO.getQuizPerID(quizNaam);
 
         Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.setTitle("Opslaan");
         alert.setHeaderText("Opslaan niet gelukt!");
 
+//  Deze hele code is obsolete nu ik een andere check op empty fields gebruik in de "doCreateUpdateQuestion" methode
+        /*
         if (questionText.isEmpty()) {
-            Stage primaryStage = null;
-            showAutoClosingWarning(primaryStage,"Voer een vraag in.");
+            showWarning("Voer een vraag in.");
             return null;
         }
         if (questionText.trim().isEmpty()) {
-            Stage primaryStage = null;
-            showAutoClosingWarning(primaryStage,"Alleen spaties als vraag is niet toegestaan.");
+            showWarning("Alleen spaties als vraag is niet toegestaan.");
             return null;
         }
         if (correctAnswer.isEmpty()) {
-            Stage primaryStage = null;
-            showAutoClosingWarning(primaryStage,"Voer een correct antwoord in.");
+            showWarning("Voer een correct antwoord in.");
             return null;
         }
         if (correctAnswer.trim().isEmpty()) {
-            Stage primaryStage = null;
-            showAutoClosingWarning(primaryStage,"Alleen spaties als correct antwoord is niet toegestaan.");
+            showWarning("Alleen spaties als correct antwoord is niet toegestaan.");
             return null;
         }
         if (wrongAnswer1.isEmpty()) {
-            Stage primaryStage = null;
-            showAutoClosingWarning(primaryStage,"Voer een eerste fout antwoord in.");
+            showWarning("Voer een eerste fout antwoord in.");
             return null;
         }
         if (wrongAnswer1.trim().isEmpty()) {
-            Stage primaryStage = null;
-            showAutoClosingWarning(primaryStage,"Alleen spaties als eerste fout antwoord is niet toegestaan.");
+            showWarning("Alleen spaties als eerste fout antwoord is niet toegestaan.");
             return null;
         }
         if (wrongAnswer2.isEmpty()) {
-            Stage primaryStage = null;
-            showAutoClosingWarning(primaryStage,"Voer een tweede fout antwoord in.");
+            showWarning("Voer een tweede fout antwoord in.");
             return null;
         }
         if (wrongAnswer2.trim().isEmpty()) {
-            Stage primaryStage = null;
-            showAutoClosingWarning(primaryStage,"Alleen spaties als tweede fout antwoord is niet toegestaan.");
+            showWarning("Alleen spaties als tweede fout antwoord is niet toegestaan.");
             return null;
         }
         if (wrongAnswer3.isEmpty()) {
-            Stage primaryStage = null;
-            showAutoClosingWarning(primaryStage,"Voer een derde fout antwoord in.");
+            showWarning("Voer een derde fout antwoord in.");
             return null;
         }
         if (wrongAnswer3.trim().isEmpty()) {
-            Stage primaryStage = null;
-            showAutoClosingWarning(primaryStage,"Alleen spaties als derde fout antwoord is niet toegestaan.");
+            showWarning("Alleen spaties als derde fout antwoord is niet toegestaan.");
             return null;
         }
-        return new Question(questionText, correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3, quiz);
+
+         */
+        Question question = new Question(questionText, correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3, quiz);
+        questionDAO.storeOne(question);
+        showWarning("Nieuwe vraag met antwoorden opgeslagen.");
+
+        return question;
     } // end createQuestion
 
     private void haalInformatieQuestionOp(Question question) {
-        titelLabel.setText("Wijzig question");
-        quizNaamTextField.setText(quizNaamTextField.getSelectedText());
-        questionNaamTextfield.setText(selectedQuestion.getQuestionText());
-        correctAnswerTextfield.setText(selectedQuestion.getCorrectAnswer());
-        wrongAnswer1Textfield.setText(selectedQuestion.getWrongAnswer1());
-        wrongAnswer2Textfield.setText(selectedQuestion.getWrongAnswer2());
-        wrongAnswer3Textfield.setText(selectedQuestion.getWrongAnswer3());
+        titelLabel.setText("Wijzig vraag");
+        quizComboBox.setValue(question.getQuiz());
+        questionNaamTextfield.setText(currentQuestion.getQuestionText());
+        correctAnswerTextfield.setText(currentQuestion.getCorrectAnswer());
+        wrongAnswer1Textfield.setText(currentQuestion.getWrongAnswer1());
+        wrongAnswer2Textfield.setText(currentQuestion.getWrongAnswer2());
+        wrongAnswer3Textfield.setText(currentQuestion.getWrongAnswer3());
     } // haalInformatieQuestionOp
 
+    // Deze alert geeft een popup met OK button. Niet wenselijk bij onze PO
     private void showAlert(Alert.AlertType type, String title, String header, String content) {
         Alert alert = new Alert(type);
         alert.setTitle(title);
@@ -170,7 +162,52 @@ public class CreateUpdateQuestionController {
         alert.showAndWait();
     } // end showAlert
 
-    private void showAutoClosingWarning(Stage owner, String message) {
+    // Alert geleend van Etienne uit User
+    // Toont foutmelding tijdelijk
+    private void showWarning(String message) {
+        warningField.setText(message);
+        warningField.setVisible(true);
+        PauseTransition pause = new PauseTransition(Duration.seconds(2));
+        pause.setOnFinished(event -> warningField.setVisible(false));
+        pause.play();
+    }
+
+    public void convertQuizToString() {
+        quizComboBox.setConverter(new StringConverter<>() {
+            @Override
+            public String toString(Quiz quiz) {
+                return quiz.getQuizName();
+            }
+
+            @Override
+            public Quiz fromString(String s) {
+                return null;
+            }
+        });
+    } // convertQuizToString
+
+    private boolean checkForEmptyFields() {
+        boolean check = false;
+        if (questionNaamTextfield.getText() == null || questionNaamTextfield.getText().trim().isEmpty()
+                || correctAnswerTextfield.getText() == null || correctAnswerTextfield.getText().trim().isEmpty()
+                || wrongAnswer1Textfield.getText() == null || wrongAnswer1Textfield.getText().trim().isEmpty()
+                || wrongAnswer2Textfield.getText() == null || wrongAnswer2Textfield.getText().trim().isEmpty()
+                || wrongAnswer3Textfield.getText() == null || wrongAnswer3Textfield.getText().trim().isEmpty()) {
+            showWarning("Vul alle velden in!");
+            check = true;
+        }
+        return check;
+    } // checkForEmptyFields
+
+    public void fillComboBox() {
+        List<Quiz> quizzes = quizDAO.getAllQuizzes();
+        quizComboBox.getItems().addAll(quizzes);
+    } // fillComboBox
+
+//    Met deze melding verdwijnt de waarschuwing na 2 seconden, tijd kan aangepast worden
+//    Geleend van ChatGPT, maar nog niet werkend
+//    Gezien de tijd, de oplossing van Etienne geleend
+/*    private void showAutoClosingWarning(Stage owner, String message) {
         Popup popup = new Popup();
         Label label = new Label(message);
         label.setStyle("-fx-background-color: yellow; -fx-text-fill: black; -fx-padding: 10; -fx-border-color: black; -fx-border-width: 1px;");
@@ -186,5 +223,7 @@ public class CreateUpdateQuestionController {
         delay.setOnFinished(e -> popup.hide());
         delay.play();
     } // showAutoClosingWarning
+
+ */
 
 } // CreateUpdateQuestionController
